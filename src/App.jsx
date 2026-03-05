d166784e072346588900a39047cef0527bd22f7a
forresttindall <forrest.tindall@gmail.com>
2026-03-05 13:15:49 -0700

    additions

--- src/App.jsx
+++ src/App.jsx
@@ -938,7 +938,7 @@ function App() {
             <p className="small-text" style={{ marginBottom: 'var(--spacing-md)' }}>CONTACT</p>
             <ul className="small-text">
               <li><a href="mailto:hello@forresttindall.com" style={{ wordBreak: 'break-all' }}>FORREST.TINDALL@GMAIL.COM</a></li>
-              <li><a href="www.instagram.com/forrest.tindall/">INSTAGRAM</a></li>
+              <li><a href="https://instagram.com/forrest.designer/">INSTAGRAM</a></li>
               <li><a href="https://www.linkedin.com/in/forrest-tindall/">LINKEDIN</a></li>
             </ul>
           </div>

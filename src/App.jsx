71ce56c0701fe527777a2681adec53193ec6f0f6
forresttindall <forrest.tindall@gmail.com>
2026-03-31 18:47:42 -0600

    link

--- src/App.jsx
+++ src/App.jsx
@@ -105,7 +105,7 @@ const SiteFooter = ({ newsletterEmail, newsletterStatus, onNewsletterEmailChange
             <p className="small-text" style={{ marginBottom: 'var(--spacing-md)' }}>CONTACT</p>
             <ul className="small-text">
               <li><a href="mailto:hello@forresttindall.com" style={{ wordBreak: 'break-all' }}>FORREST.TINDALL@GMAIL.COM</a></li>
-              <li><a href="https://instagram.com/forrest.designer/">INSTAGRAM</a></li>
+              <li><a href="https://instagram.com/forrest.tindall/">INSTAGRAM</a></li>
               <li><a href="https://www.linkedin.com/in/forrest-tindall/">LINKEDIN</a></li>
             </ul>
           </div>
